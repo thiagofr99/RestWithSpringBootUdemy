581d771d92a4de3b796d94735ff1203a868b7506
thiagofr99 <linkinht@gmail.com>
2022-04-28 23:11:17 -0300

    feat(Migrations): implementação de migrations para o projeto.

--- 06 RestWithSpringBootUdemy - Migrations/src/test/java/com/devthiagofurtado/controller/PersonControllerTest.java
+++ 06 RestWithSpringBootUdemy - Migrations/src/test/java/com/devthiagofurtado/controller/PersonControllerTest.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -37,7 +34,7 @@ class PersonControllerTest {
     private static String B = "2,0";
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
         mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
@@ -45,39 +42,4 @@ class PersonControllerTest {
         mapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
     }
 
-    @Test
-    void sum() throws Exception {
-
-        mockMvc.perform(get("/sum/"+A+"/"+B)).andExpect(status().isOk());
-    }
-
-    @Test
-    void sub() throws Exception {
-
-        mockMvc.perform(get("/sub/"+A+"/"+B)).andExpect(status().isOk());
-    }
-
-    @Test
-    void div() throws Exception {
-
-        mockMvc.perform(get("/div/"+A+"/"+B)).andExpect(status().isOk());
-    }
-
-    @Test
-    void mul() throws Exception {
-
-        mockMvc.perform(get("/mul/"+A+"/"+B)).andExpect(status().isOk());
-    }
-
-    @Test
-    void med() throws Exception {
-
-        mockMvc.perform(get("/med/"+A+"/"+B)).andExpect(status().isOk());
-    }
-
-    @Test
-    void raiz() throws Exception {
-
-        mockMvc.perform(get("/raiz/"+A)).andExpect(status().isOk());
-    }
 }
--- 06 RestWithSpringBootUdemy - Migrations/src/test/java/com/devthiagofurtado/service/PersonServiceTest.java
+++ 06 RestWithSpringBootUdemy - Migrations/src/test/java/com/devthiagofurtado/service/PersonServiceTest.java
@@ -1,10 +1,6 @@
 package com.devthiagofurtado.service;
 
-import com.devthiagofurtado.enums.Operacao;
-import com.devthiagofurtado.exception.UnsuportedMathOperationException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
