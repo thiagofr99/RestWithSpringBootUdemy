f1dd28d7588338cecf70a5cf2c079767923946fa
thiagofr99 <linkinht@gmail.com>
2022-05-01 20:26:30 -0300

    feat(Cors): Configurando o WebConfig para utilizar Cors de forma Global.

--- 12 RestWithSpringBootUdemy - CORS/src/main/java/com/devthiagofurtado/config/WebConfig.java
+++ 12 RestWithSpringBootUdemy - CORS/src/main/java/com/devthiagofurtado/config/WebConfig.java
@@ -4,9 +4,7 @@ import com.devthiagofurtado.serialization.converter.YamlJackson2HttpMessageConve
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.List;
 
@@ -48,4 +46,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("x-yaml", MEDIA_TYPE_YML);
     }
 
+    //AddCors - Global -- CorsDomain para todas as origens.
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS","HEAD","TRACE","CONNECT");
+
+    }
 }
