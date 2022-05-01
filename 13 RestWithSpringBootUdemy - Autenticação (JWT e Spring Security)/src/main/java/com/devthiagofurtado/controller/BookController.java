64cf4ec4769e9ec36a74e0d745fd3c1d2a6a84e8
thiagofr99 <linkinht@gmail.com>
2022-05-01 20:28:39 -0300

    feat(Autenticação): Commit inicial

---
+++ 13 RestWithSpringBootUdemy - Autenticação (JWT e Spring Security)/src/main/java/com/devthiagofurtado/controller/BookController.java
@@ -0,0 +1,71 @@
+package com.devthiagofurtado.controller;
+
+import com.devthiagofurtado.data.vo.BooksVO;
+import com.devthiagofurtado.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+//@CrossOrigin
+@Api(value = "Book Endpoint", description = "Description for Book", tags = {"Book Endpoint"})
+@RestController
+@RequestMapping("api/book/v1")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    //@CrossOrigin(origins = {"http://localhost:8080","http://devthiagofurtado.com"})
+    @ApiOperation(value = "Busca um registro de Book por Id.")
+    @GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
+    public BooksVO buscarPorId(@PathVariable(value = "id") Long id) {
+        BooksVO booksVO = bookService.findById(id);
+        booksVO.add(linkTo(methodOn(BookController.class).buscarPorId(id)).withSelfRel());
+        return booksVO;
+    }
+
+    //@CrossOrigin(origins = "http://localhost:8080")
+    @ApiOperation(value = "Busca todos registros de Book")
+    @GetMapping(value = {}, produces = {"application/json", "application/xml", "application/x-yaml"})
+    public List<BooksVO> buscarTodos() {
+        List<BooksVO> booksVO = bookService.findAll();
+        booksVO.forEach(p -> {
+            p.add(linkTo(methodOn(BookController.class).buscarPorId(p.getKey())).withSelfRel());
+        });
+        return booksVO;
+    }
+
+    //@CrossOrigin(origins = {"http://localhost:8080","http://devthiagofurtado.com"})
+    @ApiOperation(value = "Cria um registro de Book.")
+    @PostMapping(value = "/salvar", produces = {"application/json", "application/xml", "application/x-yaml"}
+            , consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public BooksVO salvar(@RequestBody BooksVO request) {
+        BooksVO booksVO = bookService.create(request);
+        booksVO.add(linkTo(methodOn(BookController.class).buscarPorId(booksVO.getKey())).withSelfRel());
+        return booksVO;
+    }
+
+    @ApiOperation(value = "Atualiza um registro de Book.")
+    @PutMapping(value = "/atualizar", produces = {"application/json", "application/xml", "application/x-yaml"}
+            , consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public BooksVO atualizar(@RequestBody BooksVO request) {
+        BooksVO booksVO = bookService.update(request);
+        booksVO.add(linkTo(methodOn(BookController.class).buscarPorId(booksVO.getKey())).withSelfRel());
+        return booksVO;
+    }
+
+    @ApiOperation(value = "Deleta um registro de Book.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        bookService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ 13 RestWithSpringBootUdemy - Autenticação (JWT e Spring Security)/src/main/java/com/devthiagofurtado/controller/PersonController.java
@@ -0,0 +1,68 @@
+package com.devthiagofurtado.controller;
+
+import com.devthiagofurtado.data.model.Person;
+import com.devthiagofurtado.data.vo.PersonVO;
+import com.devthiagofurtado.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Api(value = "Person Endpoint", description = "Description for person", tags = {"Person Endpoint"})
+@RestController
+@RequestMapping("api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @ApiOperation(value = "Buscar Person por Id.")
+    @GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO buscarPorId(@PathVariable(value = "id") Long id) {
+        PersonVO personVO = personService.findById(id);
+        personVO.add(linkTo(methodOn(PersonController.class).buscarPorId(id)).withSelfRel());
+        return personVO;
+    }
+
+    @ApiOperation(value = "Buscar todos registros de Person.")
+    @GetMapping(value = {}, produces = {"application/json", "application/xml", "application/x-yaml"})
+    public List<PersonVO> buscarTodos() {
+        List<PersonVO> personVOS = personService.findAll();
+        personVOS.forEach(p->{
+            p.add(linkTo(methodOn(PersonController.class).buscarPorId(p.getKey())).withSelfRel());
+        });
+        return personVOS;
+    }
+
+    @ApiOperation(value = "Cria um registro de Person.")
+    @PostMapping(value = "/salvar", produces = {"application/json", "application/xml", "application/x-yaml"}
+            , consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO salvar(@RequestBody PersonVO person) {
+        PersonVO personVO =personService.create(person);
+        personVO.add(linkTo(methodOn(PersonController.class).buscarPorId(personVO.getKey())).withSelfRel());
+        return personVO;
+    }
+
+    @ApiOperation(value = "Atualiza um registro de Person.")
+    @PutMapping( value = "/atualizar", produces = {"application/json", "application/xml", "application/x-yaml"}
+            , consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO atualizar(@RequestBody PersonVO person) {
+        PersonVO personVO = personService.update(person);
+        personVO.add(linkTo(methodOn(PersonController.class).buscarPorId(personVO.getKey())).withSelfRel());
+        return personVO;
+    }
+
+    @ApiOperation(value = "Deleta um registro de Person.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        personService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
